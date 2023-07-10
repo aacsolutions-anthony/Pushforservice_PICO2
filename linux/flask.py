1b40b67ab714524412b8ee4008d8f434e25d5beb
aacsolutions-anthony <131961269+aacsolutions-anthony@users.noreply.github.com>
2023-07-10 13:33:53 +1000

    Create flask.py

---
+++ linux/flask.py
@@ -0,0 +1,11 @@
+from flask import Flask, request
+app = Flask(__name__)
+
+@app.route('/', methods=['POST'])
+def home():
+    data = request.get_json()
+    print(data)
+    return 'JSON received\n', 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
